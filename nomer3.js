a5c5a1def1c13f7679aa13d81aeb5d2b82cf5a40
izzul112 <43197282+izzul112@users.noreply.github.com>
2021-04-13 08:28:15 +0700

    Update nomer3.js

--- nomer3.js
+++ nomer3.js
@@ -14,3 +14,22 @@ if (total < 18.5) {
 } else {
     console.log("Kegemukan Obesitas");
 }
+
+let nilai = scoreChecker(85);
+  let hasil
+if (nilai >= 90) {
+    result = "'Selamat! Anda mendapatkan nilai A.'";
+    console.log(result);
+} else if (nilai >= 80 && nilai <= 89) {
+    result = "'Anda mendapatkan nilai B.'";
+    console.log(result);
+} else if (nilai >= 70 && nilai <= 79) {
+    result = "'Anda mendapatkan nilai C.'";
+    console.log(result);
+ } else if (nilai >= 60 && nilai <= 69) {
+    result = "'Anda mendapatkan nilai D.'";
+    console.log(result);
+} else {
+    result = "'Anda mendapatkan nilai E.'";
+    console.log(result);
+}
